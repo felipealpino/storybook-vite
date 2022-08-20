481abbfa2ab4508596b3da682da40144176e269c
Felipe Gontijo Alpino <felipealpino1@gmail.com>
2022-08-20 15:33:49 -0300

    clean

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-import { createRoot } from "react-dom/client";
-const container = document.getElementById("root");
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
