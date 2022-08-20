0ce3228eabd2256ef0ba42de7aec617db1a7f5eb
Felipe Gontijo Alpino <felipealpino1@gmail.com>
2022-08-20 15:15:31 -0300

    removing error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
