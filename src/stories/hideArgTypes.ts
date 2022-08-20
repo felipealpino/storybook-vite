e11b5925da45f86c244d2da21be720ff2ab93af0
Felipe Gontijo Alpino <felipealpino1@gmail.com>
2022-08-20 13:14:53 -0300

    hide args

---
+++ src/stories/hideArgTypes.ts
@@ -0,0 +1,52 @@
+export const hideArgs = {
+  icon: {
+    table: {
+      disable: true,
+    },
+  },
+  iconfill: {
+    table: {
+      disable: true,
+    },
+  },
+  iconcolor: {
+    table: {
+      disable: true,
+    },
+  },
+  className: {
+    table: {
+      disable: true,
+    },
+  },
+  index: {
+    table: {
+      disable: true,
+    },
+  },
+  handleSetCurrent: {
+    table: {
+      disable: true,
+    },
+  },
+  accordionCurent: {
+    table: {
+      disable: true,
+    },
+  },
+  checkboxRef: {
+    table: {
+      disable: true,
+    },
+  },
+  id: {
+    table: {
+      disable: true,
+    },
+  },
+  handleUploadCallback: {
+    table: {
+      disable: true,
+    },
+  },
+};
