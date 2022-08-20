64df0daa4120a04a325169e8d01db68950ee1705
Felipe Gontijo Alpino <felipealpino1@gmail.com>
2022-08-20 20:32:32 -0300

    export all

---
+++ src/components/index.tsx
@@ -0,0 +1,29 @@
+export * from "./Accordion";
+export * from "./Accordion/Accordions";
+
+export * from "./Avatar";
+
+export * from "./BaseStyles";
+
+export * from "./Button";
+
+export * from "./Checkboxes/Checkbox";
+
+export * from "./Inputs/Input";
+
+export * from "./Pagination";
+
+export * from "./Selects/Select";
+
+export * from "./Spinner";
+
+export * from "./SwipeToggle";
+
+export * from "./Tabs/Tabs";
+export * from "./Tabs/context/TabsContext";
+
+export * from "./TextAreas/TextArea";
+
+export * from "./Toast/context/ToastContext";
+
+export * from "./Tooltip";
